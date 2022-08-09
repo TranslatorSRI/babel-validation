7d3e29c15be33f3805555a5fb1c12daab44d278b
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-08-08 22:58:47 -0400

    Fixed a bug in CHANGED_WITH_SOME_SHARED_IDENTIFIERS constraint.

--- src/main/scala/org/renci/babel/validator/Comparer.scala
+++ src/main/scala/org/renci/babel/validator/Comparer.scala
@@ -103,7 +103,7 @@ object Comparer extends LazyLogging {
         val prevIdentifiers =
           prevRecords.flatMap(_.identifiers).map(_.i).toSeq.sorted
         val overlapIdentifiers =
-          identifiers.flatten.intersect(prevIdentifiers.flatten)
+          identifiers.flatten.intersect(prevIdentifiers.flatten).filterNot(_ != id)
 
         if (identifiers == prevIdentifiers) "CHANGED_WITH_IDENTICAL_IDENTIFIERS"
         else if (overlapIdentifiers.nonEmpty)
