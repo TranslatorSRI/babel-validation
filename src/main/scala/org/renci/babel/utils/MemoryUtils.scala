8494d14e3a4308bc05ce5992a0f0a1ef70f8f3d1
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-08-23 22:17:30 -0400

    Improved memory output.

--- src/main/scala/org/renci/babel/utils/MemoryUtils.scala
+++ src/main/scala/org/renci/babel/utils/MemoryUtils.scala
@@ -8,8 +8,11 @@ object MemoryUtils {
 
     val memoryUsed = runtime.totalMemory() - runtime.freeMemory()
 
+    val memoryUsedOfAvailPercent = memoryUsed.toDouble/runtime.totalMemory()*100
+    val memoryUsedOfMaxPercent = memoryUsed.toDouble/runtime.maxMemory()*100
+
     f"${bytesToGB(runtime.freeMemory())}%.2f GB free out of ${bytesToGB(
         runtime.totalMemory()
-      )}%.2f GB (${bytesToGB(memoryUsed)}%.2f GB used out of ${bytesToGB(runtime.maxMemory())}%.2f GB max)"
+      )}%.2f GB (${bytesToGB(memoryUsed)}%.2f GB used out of ${bytesToGB(runtime.maxMemory())}%.2f GB max: ${memoryUsedOfAvailPercent}%.2f%% of avail, ${memoryUsedOfMaxPercent}%.2f%% of max)"
   }
 }
