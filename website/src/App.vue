c8446bec384f932e5cec52bc80f000c34911c85c
Gaurav Vaidya <gaurav@renci.org>
2022-09-09 17:48:16 -0400

    Added Bootstrap, added basic interface.

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,91 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <h1>Babel Validation</h1>
+  <p>
+    <a href="https://github.com/TranslatorSRI/babel">Babel</a> is the program that generates the datasets of
+    interrelated identifiers that underlies <a href="https://github.com/TranslatorSRI/NodeNormalization">Node Normalization</a>
+    and <a href="https://github.com/TranslatorSRI/NameResolution">Name Resolution</a>. This website is intended to provide
+    a single place where tests can be executed against multiple versions of these tools.
+  </p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h2>Tests</h2>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <b-table striped hover :items="testResults" :fields="test_fields">
+  </b-table>
 
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<script>
+import {BTable} from "bootstrap-vue-3";
+export default {
+  components: {BTable},
+  data () {
+    return {
+      nodeNormEndpoints: {
+        "NodeNorm-RENCI-exp": "https://nodenormalization-dev.apps.renci.org/1.3",
+        "NodeNorm-RENCI-dev": "https://nodenormalization-sri.renci.org/1.3",
+        "NodeNorm-ITRB-prod": "https://nodenorm.transltr.io/1.3/"
+      },
+      tests: fetch("")
+    }
+  },
+  computed: {
+    test_fields() {
+      /* Return a list of fields to display in the tests list */
+      return [ "Test", ...Object.keys(this.nodeNormEndpoints)]
+    },
   }
 }
-</style>
+</script>
