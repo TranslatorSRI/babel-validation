46584f399cb4a8514bf34d07e7017a1db3572df7
Gaurav Vaidya <gaurav@renci.org>
2023-06-07 15:49:28 -0400

    Made DATA_DIR configurable.

--- souffle/generate-combined.sh
+++ souffle/generate-combined.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SYNONYM_FILENAMES=(../data/2023may18/synonyms/*.txt.gz)
+DATA_DIR=../data/2023may18/synonyms
+SYNONYM_FILENAMES=($DATA_DIR/*.txt.gz)
 
 echo ${SYNONYM_FILENAMES[*]}
 
 # Combine files into a single giant combined.tsv.gz file.
-gunzip -c ${SYNONYM_FILENAMES[*]} | jq -r '[ .curie, (.types | join("|")), .preferred_name ] | @tsv' |gzip > combined.tsv.gz
+gunzip -c ${SYNONYM_FILENAMES[*]} | jq -r '[ .curie, (.types | join("|")), .preferred_name ] | @tsv' |gzip > $DATA_DIR/combined.tsv.gz
