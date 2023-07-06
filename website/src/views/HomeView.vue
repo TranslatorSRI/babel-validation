2dd2a2944f1388fd9c6af30e09561340f7c3b087
Gaurav Vaidya <gaurav@renci.org>
2023-07-06 13:35:50 -0400

    Moved NodeNorm validation to /nodenorm.

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Babel Validator</h1>
+
+  <p>
+    <a href="https://github.com/TranslatorSRI/babel">Babel</a> is the program that generates the datasets of
+    interrelated identifiers that underlies <a href="https://github.com/TranslatorSRI/NodeNormalization">Node Normalization</a>
+    and <a href="https://github.com/TranslatorSRI/NameResolution">Name Resolution</a>. This website is intended to provide
+    a single place where tests can be executed against multiple versions of these tools.
+  </p>
+
+  <ul>
+    <li><a href="/nodenorm">Node Normalization Validator</a></li>
+
+  </ul>
 </template>
+
+<script setup>
+</script>
