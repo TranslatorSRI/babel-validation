82f801f5044a303d6665ea04b13540c214912940
Gaurav Vaidya <gaurav@renci.org>
2023-07-20 17:59:46 -0400

    Fixed code.

--- souffle/generate-combined.sh
+++ souffle/generate-combined.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-DATA_DIR=../data/2023may18/synonyms
-SYNONYM_FILENAMES=($DATA_DIR/*.txt.gz)
+DATE=2023jul13
+DOWNLOAD_URL=https://stars.renci.org/var/babel_outputs/$DATE/synonyms/
+DATA_DIR=../data/$DATE/synonyms
+mkdir -p $DATA_DIR
+wget --recursive --no-parent --no-clobber --no-host-directories --cut-dirs=4 -R "index.html" $DOWNLOAD_URL -P $DATA_DIR
 
+SYNONYM_FILENAMES=($DATA_DIR/*.txt.gz)
 echo ${SYNONYM_FILENAMES[*]}
 
 # Combine files into a single giant combined.tsv.gz file.
