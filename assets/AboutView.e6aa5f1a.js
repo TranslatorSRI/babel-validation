e3efb2c396a0e3e940539652239721560f8bce0f
gaurav <gaurav@ggvaidya.com>
2024-01-04 06:12:22 +0000

    Deploying to gh-pages from @ TranslatorSRI/babel-validation@82016d415976b012d7e952f0e8ebd24ef5366835 🚀

---
+++ assets/AboutView.e6aa5f1a.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,f as o,j as s}from"./index.24b16168.js";const _={},c={class:"about"},n=s("h1",null,"This is an about page",-1),a=[n];function i(r,u){return t(),o("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
