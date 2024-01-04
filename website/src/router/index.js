91f02784fe4db92ab2e193d11e08de178198cd37
Gaurav Vaidya <gaurav@ggvaidya.com>
2024-01-04 00:51:05 -0500

    Added base URL to Router.

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -6,6 +6,7 @@ import AutocompleteValidator from "@/views/AutocompleteValidator.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  base: import.meta.env.BASE_URL,
   routes: [
     {
       path: "/",
