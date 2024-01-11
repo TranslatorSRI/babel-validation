417ee2921aa490d57910d70b42cccd1174383f0f
Gaurav Vaidya <gaurav@ggvaidya.com>
2024-01-10 23:06:52 -0500

    First stab at an extant Autocomplete with Name Resolver.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    'base': '/babel-validation'
+  'base': '/babel-validation/',
+  integrations: [vue()]
 });
