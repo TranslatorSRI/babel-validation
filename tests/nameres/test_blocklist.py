de7b236ede328e06a22b93ddeca59c088574ec43
Gaurav Vaidya <gaurav@renci.org>
2024-02-26 12:27:30 -0500

    Parametrize blocklist entries.

--- tests/nameres/test_blocklist.py
+++ tests/nameres/test_blocklist.py
@@ -54,7 +54,7 @@ class BlocklistEntry:
         )
 
 
-def load_blocklist_from_gsheet(google_sheet_id):
+def load_blocklist_from_gsheet():
     """
     Load the Blocklist from a Google Sheet.
 
@@ -75,8 +75,12 @@ def load_blocklist_from_gsheet(google_sheet_id):
 
     return rows
 
+# Parameterize blocklist entries.
+blocklist_entries = load_blocklist_from_gsheet()
 
-def test_check_blocklist_entry(target_info):
+
+@pytest.mark.parametrize("blocklist_entry", blocklist_entries)
+def test_check_blocklist_entry(target_info, blocklist_entry):
     """
     Test whether a NameRes instance has blocked every item from a blocklist.
 
@@ -85,30 +89,30 @@ def test_check_blocklist_entry(target_info):
     nameres_url = target_info['NameResURL']
     nameres_url_reverse_lookup = nameres_url + 'reverse_lookup'
 
-    blocklist_gsheet_id = target_info['BlocklistGSheetID']
-
-    # We only support the Google Sheet blocklist for now.
-    if not blocklist_gsheet_id:
+    # Only "blocked" entries are considered, since most of the spreadsheet is things we decided _not_ to block.
+    if blocklist_entry.Blocked == 'y':
+        flag_expect_present = False
+    elif blocklist_entry.Blocked == 'n':
+        flag_expect_present = True
+    else:
+        logging.info(f"Skipping blocklist entry as it is not asserted to be blocked: {blocklist_entry}")
         return
 
-    blocklist_entries = load_blocklist_from_gsheet(blocklist_gsheet_id)
+    if flag_expect_present and not blocklist_entry.CURIE:
+        # We've got a bunch of these, just ignore them.
+        pytest.skip(f"Blocklist entry expected to not be blocked, but no CURIE provided: {blocklist_entry}")
 
-    assert len(blocklist_entries) > 0, f"No blocklist entries found in Google Sheet {blocklist_gsheet_id}"
+    assert blocklist_entry.CURIE, f"Blocklist entry claims to be blocked, but does not have a CURIE: {blocklist_entry}"
 
-    for entry in blocklist_entries:
-        # Only "blocked" entries are considered, since most of the spreadsheet is things we decided _not_ to block.
-        if not entry.is_blocked():
-            logging.info(f"Skipping blocklist entry as it is not asserted to be blocked: {entry}")
-            continue
+    # Someday we would like to do this with the query as well, but that would require some work.
+    # So we only test the CURIE for now.
+    response = requests.get(nameres_url_reverse_lookup, params={
+        'curies': blocklist_entry.CURIE,
+    })
+    assert response.ok
+    result = response.json()[blocklist_entry.CURIE]
 
-        assert entry.CURIE, f"Blocklist entry claims to be blocked, but does not have a CURIE: {entry}"
-
-        # Someday we would like to do this with the query as well, but that would require some work.
-        # So we only test the CURIE for now.
-        response = requests.get(nameres_url_reverse_lookup, params={
-            'curies': entry.CURIE,
-        })
-        assert response.ok
-        result = response.json()[entry.CURIE]
-
-        assert result == {}, f"Expected {entry.CURIE} to be absent on {nameres_url_reverse_lookup}, but found: {result}"
+    if flag_expect_present:
+        assert result != {}, f"Expected {blocklist_entry.CURIE} to be present on {nameres_url_reverse_lookup}, but found: {result}"
+    else:
+        assert result == {}, f"Expected {blocklist_entry.CURIE} to be absent on {nameres_url_reverse_lookup}, but found: {result}"
