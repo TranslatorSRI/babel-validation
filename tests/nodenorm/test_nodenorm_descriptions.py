23142a1f0016850e834bcc1adeeeb57efd8aed01
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-11-29 21:57:53 -0500

    Got tests to pass on NodeNorm Exp.

--- tests/nodenorm/test_nodenorm_descriptions.py
+++ tests/nodenorm/test_nodenorm_descriptions.py
@@ -8,41 +8,60 @@ IDENTIFIERS_WITH_DESCRIPTIONS = {
     'MESH:D014867',
     'NCIT:C34373',
     'NCBIGene:1756',
-    'UMLS:C0665297',
 }
+
+IDENTIFIERS_WITHOUT_DESCRIPTIONS = {
+    'UMLS:C0665297',        # natalizumab
+}
+
 @pytest.mark.parametrize('curie', IDENTIFIERS_WITH_DESCRIPTIONS)
 def test_descriptions(target_info, curie):
     """ Look up a few identifiers known to have descriptions. """
     nodenorm_url = target_info['NodeNormURL']
 
-    response = requests.get(f'https://{nodenorm_url}/get_normalized_nodes', params={'curie': curie})
+    # Make sure we get descriptions if we set the description flag for this CURIE.
+    response = requests.post(nodenorm_url + 'get_normalized_nodes', json={
+        'curies': [curie],
+        'description': True,
+    })
     response.raise_for_status()
     result = response.json()
 
     assert curie in result
 
     curie_result = result[curie]
-    assert 'description' in curie_result['identifier']
-    assert curie_result['identifier']['description'] != ''
+    assert 'description' in curie_result['id']
+    assert curie_result['id']['description'] != ''
 
     assert 'descriptions' in curie_result
     assert len(curie_result['descriptions']) > 0
 
+    # Make sure we don't get this response if the description flag is false.
+    response = requests.post(nodenorm_url + 'get_normalized_nodes', json={
+        'curies': [curie],
+        'description': False,
+    })
+    response.raise_for_status()
+    result = response.json()
 
-IDENTIFIERS_WITHOUT_DESCRIPTIONS = {
-    'UMLS:C0665297',        # natalizumab
-}
+    assert curie in result
+    curie_result = result[curie]
+    assert 'description' not in curie_result['id']
+    assert 'descriptions' not in curie_result
+
+
 @pytest.mark.parametrize('curie', IDENTIFIERS_WITHOUT_DESCRIPTIONS)
-def test_no_descriptions(target_info, curie):
+@pytest.mark.parametrize('description_flag', ['false', 'true'])
+def test_no_descriptions(target_info, curie, description_flag):
     """ Look up a few identifiers that don't have descriptions. """
     nodenorm_url = target_info['NodeNormURL']
 
-    response = requests.get(f'https://{nodenorm_url}/get_normalized_nodes', params={'curie': curie})
+    response = requests.get(nodenorm_url + 'get_normalized_nodes', params={'curie': curie, 'description': description_flag})
     response.raise_for_status()
     result = response.json()
 
     assert curie in result
 
     curie_result = result[curie]
-    assert 'description' not in curie_result['identifier']
+    assert 'description' not in curie_result['id']
     assert 'descriptions' not in curie_result
